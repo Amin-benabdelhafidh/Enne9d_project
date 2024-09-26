aa36fdf852f00ab31144ee5d57df7f1bb8b408b7
Amin Ben Abdelhafidh <benabdelhafidhamine3@gmail.com>
2024-09-26 17:05:45 +0100

    initial

---
+++ Enneqd/src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="bg-slate-50">
+        <p>ENNE9D is a free, open source platform meant to bring together the tunisian movies/series lovers inorder to help improve the tunisian TV shows by contributing with reviews.</p>
+        <p class="text-center">&copy; all rights reserved 2024</p>
+    </footer>
+</template>
+
+<style scoped></style>
---
+++ Enneqd/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="w-full bg-orange-400 p-0">
+        <div class="flex flex-row items-center">
+            <div class="my-2 basis-1/3 pl-5">
+                <h1 class="text-lg font-medium">Enne9d</h1>
+            </div>
+
+            <div class="basis-1/3">
+                <input class="w-full" type="search" placeholder="search...">
+            </div>
+
+            <div class="basis-1/3 pr-1">
+                <p class="float-right">SignIn</p>
+            </div>
+        </div>
+        <div class="w-full sm:"> 
+            <ul class="flex flex-row items-center justify-center gap-2 text-nowrap mb-10">
+                <li>Hot!</li>
+                <li>Top Rated</li>
+                <li>Trending Reviews</li>
+            </ul>
+        </div>
+    </header>
+
+</template>
+
+<style scoped>
+</style>
